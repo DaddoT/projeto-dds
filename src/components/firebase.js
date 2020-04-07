4857f24804100d066d464ca917304f75febfae60
Daddo Targus <daddotargus@MacBook-Pro-de-Daddo.local>
2020-04-07 19:29:22 -0300

    second-commit

---
+++ src/components/firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from 'firebase';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAU0_Fevv9dZiArXWwzoxd_7nVnTP0HqfE",
+    authDomain: "projeto-dds-3e0d0.firebaseapp.com",
+    databaseURL: "https://projeto-dds-3e0d0.firebaseio.com",
+    projectId: "projeto-dds-3e0d0",
+    storageBucket: "projeto-dds-3e0d0.appspot.com",
+    messagingSenderId: "509888701387",
+    appId: "1:509888701387:web:64c044335319bada488c4e",
+    measurementId: "G-9CY5ZTLH7P"
+  };
+
+  // APP instance
+export const app = firebase.initializeApp(firebaseConfig);
+// DB instance
+export const database = firebase.firestore();
