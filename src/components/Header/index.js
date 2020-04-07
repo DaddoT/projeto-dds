2e443d77aa0896a834c0af42625fecca08c251ac
Daddo Targus <daddotargus@MacBook-Pro-de-Daddo.local>
2020-04-07 20:24:30 -0300

    third-commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,26 @@
 import React from "react";
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
 import "./styles.css";
 
-const Header = () => (
-    <header id ="main-header"> Customer Relationship </header>
-);
-
-export default Header;
+const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(0),
+      },
+    },
+  }));
+  
+  export default function TextButtons() {
+    const classes = useStyles();
+  
+    return (
+      <div className={classes.root}>
+        <header id ="main-header"> 
+        <Button > <p>Default Link </p></Button>
+        <Button > <p>Default Link </p></Button>
+        <Button > <p>Default Link </p></Button>
+        </header>
+      </div>
+    );
+  }
