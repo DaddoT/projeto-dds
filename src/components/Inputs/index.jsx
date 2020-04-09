428e7da52944671f1e9257b0b5c8a85be116e4bd
Daddo Targus <daddotargus@MacBook-Pro-de-Daddo.local>
2020-04-08 21:18:37 -0300

    sixth-commit

--- src/components/Inputs/index.jsx
+++ src/components/Inputs/index.jsx
@@ -3,13 +3,32 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import "./styles.css";
 
-export default function Inputs() {
+// export default function Inputs() {
+
+  
+  import { makeStyles } from '@material-ui/core/styles';
+  // import TextField from '@material-ui/core/TextField';
+  
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: '35ch',
+    },
+  }));
+  
+  export default function LayoutTextFields() {
+    const classes = useStyles();
 
 return (
 <div id="inputs">
-<form className="Inputs" noValidate autoComplete="off">
-  <TextField label="Email" variant="filled" type="email"/> <br></br><br></br>
-  <TextField label="Senha" variant="filled" type="password"/> <br></br><br></br>
+<form className={classes.textField} noValidate autoComplete="off" >
+  <TextField label="Email" variant="filled" type="email" fullWidth/> <br></br><br></br>
+  <TextField label="Senha" variant="filled" type="password" fullWidth/> <br></br><br></br>
   <div id="buttons">
   <Button variant="contained" color="default">
   Sign in
@@ -17,7 +36,8 @@ return (
   <div id="divider"/>
   <Button variant="contained" color="primary">
   Sign up
-  </Button>
+  </Button> <br></br> <br></br>
+  <a href="google.com">Esqueci minha senha</a>
   </div>
 </form>
 </div>
