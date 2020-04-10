2dd48e69e6963dbf04acadfab3f36be4b6b31daf
Daddo Targus <daddotargus@MacBook-Pro-de-Daddo.local>
2020-04-10 02:58:40 -0300

    multipage

--- src/App.js
+++ src/App.js
@@ -2,15 +2,55 @@ import React from 'react';
 import Header from './components/Header';
 import Inputs from './components/Inputs';
 import "./styles.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-const App = () => (
-  <div className="App">
+export default function App() {
+return (
+<Router>
+  {/* A <Switch> looks through its children <Route>s and
+      renders the first one that matches the current URL. */}
+  <Switch>
+    <Route path="/about">
+      <About />
+    </Route>
+    <Route path="/Pricing">
+      <Pricing />
+    </Route>
+    <Route path="/">
+      <Home />
+    </Route>
+  </Switch>
+</Router>
+);
+}
+
+function Home() {
+return (
+<div className="App">
   <Header />
   <Inputs />
- </div>
+  </div>
 );
+}
+
+function About() {
+return (
+  <Header />
+);
+}
+
+function Pricing() {
+return (
+  <Header />
+);
+}
 
 
-export default App;
+
+// export default App;
 
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import Button from '@material-ui/core/Button';
 import "./styles.css";
 import logo from './logoHeader.png';
-// import Typography from '@material-ui/core/Typography';
-// import Container from '@material-ui/core/Container';
+import { Link } from "react-router-dom";
 
   export default function Header() {
 
@@ -11,8 +10,9 @@ import logo from './logoHeader.png';
       <div className="Header">
         <header id ="main-header" > 
         <img src={logo} alt="logo" />
-        <Button > <p>About </p></Button>
-        <Button > <p>Pricing </p></Button>
+        <Button > <Link to="/home"><p>Home</p></Link></Button>
+        <Button > <Link to="/about"><p>About</p></Link></Button>
+        <Button > <Link to="/pricing"><p>Pricing </p></Link></Button>
         <Button > <p>Link </p></Button>
         </header>
               
