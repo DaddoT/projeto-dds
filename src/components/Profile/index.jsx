dd0c8c31cc2386c5881adeb2176422ea27c2103e
Daddo Targus <daddotargus@MacBook-Pro-de-Daddo.local>
2020-04-13 21:11:41 -0300

    profile-page

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Button} from "@material-ui/core";
 import { fb, database, auth } from '../firebase.js';
 import { Link, useHistory } from "react-router-dom";
+import './stylesProfile.css';
 
 export default function ProfilePage(){
   var history = useHistory();
@@ -20,9 +21,30 @@ export default function ProfilePage(){
     }).catch();
 
   };
-  
+
   return (
-    <Button variant="contained" color="default" onClick={()=>signOut()}>Deslogar</Button>
+<div className="Page">
+  <div className="Options">
+
+  <div className="ButtonsDash">
+    <Button color="default" ><p4>Registrar empresa</p4></Button>
+    <div id="divider" />
+    <Button color="default" ><p4>Registrar empresarial</p4></Button>
+    </div>
+      
+      <div className="ButtonsDeslog">
+    <Button color="default"  onClick={()=>signOut()}><p4>Deslogar</p4></Button> <br></br> <br></br> 
+      </div>
+
+  </div>
+
+  <div className="dashboard" >
+
+    
+
+  </div>  
+
+</div>
   );
-};
+}
 
