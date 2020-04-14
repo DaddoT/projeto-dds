e9f9aa868b3fefcc0bee4f08d817b6b0960f6f6b
Daddo Targus <daddotargus@MacBook-Pro-de-Daddo.local>
2020-04-14 19:47:06 -0300

    cards-new

--- src/components/PricingCard/index.jsx
+++ src/components/PricingCard/index.jsx
@@ -10,35 +10,35 @@ import GridList from '@material-ui/core/Grid';
 const useStyles = makeStyles({
   card1: {
     minHeight: 600,
-    maxWidth: 350,
-    marginLeft: 80,
-    marginTop: 20,
+    minWidth: 350,
+    // marginLeft: 80,
+    // marginTop: 20,
     backgroundColor: '#282828',
   },
   card2: {
     minHeight: 600,
-    maxWidth: 350,
-    marginLeft: 533, 
-    marginTop: -635,
+    minWidth: 350,
+    // marginLeft: 533, 
+    // marginTop: -635,
     backgroundColor: '#282828',
   },
   card3: {
     minHeight: 600,
-    maxWidth: 350,
-    marginLeft: 980,
-    marginTop: -600,
+    minWidth: 350,
+    // marginLeft: 980,
+    // marginTop: -600,
     backgroundColor: '#282828',
   },
-  bullet: {
+  cards: {
+    marginTop: 20,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  divider: {
+    minWidth: '100px',
+    height: 'auto',
     display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
   },
 });
 
@@ -46,8 +46,7 @@ export default function SimpleCard() {
   const classes = useStyles();
   
   return (
-      <div className="Cards">
- <GridList cols={3} >         
+  <div className={classes.cards}>        
 <Card className={classes.card1}>
     <CardContent>
         
@@ -55,7 +54,7 @@ export default function SimpleCard() {
 
     </CardContent>
 </Card> 
-    <br></br> <br></br>
+  <div className={classes.divider} />
 <Card className={classes.card2}>
     <CardContent>
  
@@ -63,6 +62,7 @@ export default function SimpleCard() {
 
     </CardContent>
 </Card>
+<div className={classes.divider} />
 <Card className={classes.card3}>
     <CardContent>
  
@@ -70,8 +70,6 @@ export default function SimpleCard() {
 
     </CardContent>
 </Card>
-</GridList>
-
 </div>
   );
 }
