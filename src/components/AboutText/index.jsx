223d0cfc13eefd0d39c0f064f81328b8a3a4d035
Daddo Targus <daddotargus@MacBook-Pro-de-Daddo.local>
2020-04-15 11:49:59 -0300

    corrige-erros

--- src/components/AboutText/index.jsx
+++ src/components/AboutText/index.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 
 import './styles.css';
 
 export default function AboutText() {
+
+
+  const useStyles = makeStyles((theme) => ({
+    p: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+      fontFamily: 'monospace',
+      color: 'white',
+      fontSize: '15px',
+    },
+    }));
+
+    const classes = useStyles()
+
     return(
         <div>
         <h1>Lorem ipsum dolor sit amet </h1> <br></br>
 
-        <p3>a</p3>
+        <p className={classes.p}>a</p>
         </div>
     );
 }
